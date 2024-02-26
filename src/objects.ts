61c71911bcc05169b4fb366e93d2e16929651909
John Fulkerson <johnfulky@mac.com>
2024-02-25 20:57:58 -0500

    implemented toMarkdown, not sure if using .trim is best way or not

--- src/objects.ts
+++ src/objects.ts
@@ -76,7 +76,9 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    return `# ${question.name}\n${question.body}\n${question.options
+        .map((option: string): string => `- ${option}`)
+        .join("\n")}`.trim();
 }
 
 /**
