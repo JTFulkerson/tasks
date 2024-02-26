168aef7a72f1ac90e66b40b7fea7eb2bb4fd9918
John Fulkerson <johnfulky@mac.com>
2024-02-25 20:43:20 -0500

    Completed and tested isValid and toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -43,7 +43,9 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    return question.type === "multiple_choice_question"
+        ? question.options.includes(answer)
+        : true;
 }
 
 /**
@@ -53,7 +55,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.substring(0, 10)}`;
 }
 
 /**
