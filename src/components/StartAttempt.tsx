35e43799bd4767046acb3ec1f881f90f6849277d
John Fulkerson <johnfulky@mac.com>
2024-03-03 12:58:23 -0500

    StartAttempt is passing 10 tests but still failing 16 after following all directions. Appears it is expecing a number that is not listed

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,31 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    function startQuiz(): void {
+        setInProgress(true);
+        setAttemptsLeft(attemptsLeft - 1);
+    }
+
+    return (
+        <div>
+            <Button
+                onClick={startQuiz}
+                disabled={inProgress || attemptsLeft === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={() => setInProgress(false)} disabled={!inProgress}>
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => setAttemptsLeft(attemptsLeft + 1)}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+        </div>
+    );
 }
