179017df8f1cf731e4106d367f1dbc0e22ebe35f
John Fulkerson <johnfulky@mac.com>
2024-03-04 18:39:52 -0500

    implemented Give Attempts and pass tests

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [requestedAttempts, setRequestedAttempts] = useState<string>();
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <p>Attempts Left: {attemptsLeft}</p>
+            <Form.Group controlId="requestAdditionalAttempts">
+                <Form.Label>Enter Number Of Additional Requests:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requestedAttempts}
+                    onChange={(e) => setRequestedAttempts(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                onClick={() => setAttemptsLeft(attemptsLeft - 1)}
+                disabled={attemptsLeft === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttemptsLeft(
+                        attemptsLeft + parseInt(requestedAttempts || "0")
+                    )
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
