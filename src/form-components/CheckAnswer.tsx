09b99ca233a78b6125b5309a3919bd06b588740d
John Fulkerson <johnfulky@mac.com>
2024-03-04 18:22:59 -0500

    implemented and passing all CheckAnswer Tests

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
     return (
         <div>
-            <h3>Check Answer</h3>
+            <h3>Check Answer {userAnswer === expectedAnswer ? "✔️" : "❌"}</h3>
+            <Form.Group>
+                <Form.Label>Enter Answer:</Form.Label>
+                <Form.Control
+                    value={userAnswer}
+                    onChange={(e) => setUserAnswer(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
         </div>
     );
 }
