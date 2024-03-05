de5e7776349f1226a18c14994ffb19d7bc70de9e
John Fulkerson <johnfulky@mac.com>
2024-03-04 19:22:17 -0500

    implemented MultipleChoiceQuestion, passing all 134 tests for task

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setChosenAnswer] = useState<string>(options[0]);
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <h3>
+                Multiple Choice Question{" "}
+                {userAnswer === expectedAnswer ? "✔️" : "❌"}
+            </h3>
+            <Form.Group controlId="multipleChoiceQuestion">
+                <Form.Label>Choose Answer:</Form.Label>
+                <Form.Select
+                    value={userAnswer}
+                    onChange={(e) => setChosenAnswer(e.target.value)}
+                >
+                    {options.map((color: string) => (
+                        <option key={color} value={color}>
+                            {color}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
         </div>
     );
 }
